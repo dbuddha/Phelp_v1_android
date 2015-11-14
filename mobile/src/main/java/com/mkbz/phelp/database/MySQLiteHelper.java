0e42efa0bf7293712671087f4ca147804dbc0d2f
zweii <marcelo_silva.313@hotmail.com>
2015-11-14 00:54:16 +0000

    no errors

---
+++ mobile/src/main/java/com/mkbz/phelp/database/MySQLiteHelper.java
@@ -0,0 +1,8 @@
+package com.mkbz.phelp.database;
+
+/**
+ * Created by MARCELO on 14-11-2015.
+ */
+public class MySQLiteHelper {
+
+}
--- mobile/src/main/java/com/mkbz/phelp/datasource/ModelDataSource.java
+++ mobile/src/main/java/com/mkbz/phelp/datasource/ModelDataSource.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 
+import com.mkbz.phelp.model.Model;
+
 /**
  * Created by MARCELO on 14-11-2015.
  */
 
-public abstract class ModelDataSource<Model> extends DataSource<Model> {
+public abstract class ModelDataSource<T extends Model> extends DataSource<Model> {
 
     protected String tableName;
     protected String idName;
@@ -34,12 +36,14 @@ public abstract class ModelDataSource<Model> extends DataSource<Model> {
 
     @Override
     protected long getIdFrom(Model m) {
-        return m.getId();
+        //return m.getId();
+        return 0;
     }
 
 
     @Override
     protected ContentValues getValues(Model m) {
-        return m.getContentValues();
+        //return m.getContentValues();
+        return null;
     }
 }
