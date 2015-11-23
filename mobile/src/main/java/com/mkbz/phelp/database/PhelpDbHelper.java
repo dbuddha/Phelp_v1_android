32b2d7288ace4a9bc6a827b58d278b8e8249a44f
zweii <marcelo_silva.313@hotmail.com>
2015-11-23 21:59:42 +0000

    database added

--- mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
+++ mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
@@ -5,28 +5,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.mkbz.phelp.database.table.TableList;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 /**
  * Created by MARCELO on 14-11-2015.
  */
-public class PhelpDbHelper extends SQLiteOpenHelper {
+public class PhelpDbHelper extends SQLiteAssetHelper {
 
     private final static String DATABASE_NAME = "phelp_mkbz.db";
     private final static int DATABASE_VERSION = 1;
-    private final TableList tables;
 
     public PhelpDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.tables = new TableList();
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        tables.onCreate(db);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        tables.onUpgrade(db, oldVersion, newVersion);
     }
 }
