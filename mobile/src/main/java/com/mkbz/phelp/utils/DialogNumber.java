e3a7b0cf5e06db6dbf506d3355ff223bc5325df9
Presumably <joseflopez92@gmail.com>
2015-12-21 20:09:14 +0000

    Dialog

---
+++ mobile/src/main/java/com/mkbz/phelp/utils/DialogNumber.java
@@ -0,0 +1,34 @@
+package com.mkbz.phelp.utils;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+
+/**
+ * Created by Jos on 21/12/2015.
+ *
+ */
+public class DialogNumber extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        }
+                );
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
