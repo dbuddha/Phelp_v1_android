05bf5076743edfc1fd5e9337c83ba6cf690fb3ed
Presumably <joseflopez92@gmail.com>
2015-12-21 19:15:55 +0000

    0.9.2

--- mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
+++ mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
@@ -10,9 +10,10 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class PhelpDbHelper extends SQLiteAssetHelper {
 
     private final static String DATABASE_NAME = "phelp_mkbz.db";
-    private final static int DATABASE_VERSION = 2;
+    private final static int DATABASE_VERSION = 3;
 
     public PhelpDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        setForcedUpgrade();
     }
 }
