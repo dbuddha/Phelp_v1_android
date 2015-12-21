ff881312596db1bff651e44659173a736232472d
Presumably <joseflopez92@gmail.com>
2015-12-21 22:54:19 +0000

    Dialog

--- mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
+++ mobile/src/main/java/com/mkbz/phelp/database/PhelpDbHelper.java
@@ -10,7 +10,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class PhelpDbHelper extends SQLiteAssetHelper {
 
     private final static String DATABASE_NAME = "phelp_mkbz.db";
-    private final static int DATABASE_VERSION = 3;
+    private final static int DATABASE_VERSION = 4;
 
     public PhelpDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
